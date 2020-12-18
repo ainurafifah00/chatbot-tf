a45f4f74d87dd08f795d8ce37a9e23342a6a3626
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2020-12-18 16:50:03 +0800

    Tokenize each word in a sentence using nltk

--- chatbot.py
+++ chatbot.py
@@ -11,3 +11,23 @@ import tflearn
 import random
 import json
 
+with open('intents.json') as json_data:
+	intents = json.load(json_data)
+
+#print(intents)
+
+words = []
+classes = []
+documents = []
+ignore = ['?']
+#loop through each sentence in the intent's pattern
+for intent in intents['intents']:
+	for pattern in intent['patterns']:
+		#tokenize each word in sentence
+		w = nltk.word_tokenize(pattern)
+		print(w)
+		#add word to words list
+		words.extend(w)
+		documents.append((w,intent['tag']))
+		if intent['tag'] not in classes:
+			classes.append(intent['tag'])
