cf0e19f99a14c50d30a21cdd93f731c81a0dd13b
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2020-12-19 15:35:39 +0800

    Performed stemming

--- chatbot.py
+++ chatbot.py
@@ -25,9 +25,33 @@ for intent in intents['intents']:
 	for pattern in intent['patterns']:
 		#tokenize each word in sentence
 		w = nltk.word_tokenize(pattern)
-		print(w)
+		#print(w)
 		#add word to words list
 		words.extend(w)
 		documents.append((w,intent['tag']))
 		if intent['tag'] not in classes:
 			classes.append(intent['tag'])
+
+#print(documents)
+#print(classes)
+
+#Perform stemming and lower each word and remove duplicates
+words = [stemmer.stem(w.lower()) for w in words if w not in ignore]
+words = sorted(list(set(classes)))
+
+#print(words)
+
+#remove duplicate classes
+classes = sorted(list(set(classes)))
+
+
+print(len(documents), "documents")
+print(len(classes), "classes", classes)
+print(len(words), "unique stemmed words", words)
+
+
+
+
+
+
+
