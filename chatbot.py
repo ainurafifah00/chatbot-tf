f16aeca9ada7a035b2ecdb933fc7f68328206d58
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2020-12-22 11:21:08 +0800

    Training neural network

--- chatbot.py
+++ chatbot.py
@@ -91,7 +91,7 @@ print("Y", train_y)
 
 
 #resetting underlying graph data
-tf.reset_default_graph()
+tf.compat.v1.reset_default_graph()
 
 #Building neural network
 net = tflearn.input_data(shape=[None, len(train_x[0])])
@@ -99,6 +99,13 @@ net = tflearn.fully_connected(net, 10)
 net = tflearn.fully_connected(net, 10)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
 net = tflearn.regression(net)
+
+#defining model and setting up tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+#Start training
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
 
 
 
